637c056a685bb1f97cd502b10a7e3fe8539a509e
Matvii Fedyk <mfedyk@griddynamics.com>
2022-03-16 14:01:17 +0200

    init commit

---
+++ src/LessonOneSimpleTicTacToe/MainTictacToe.java
@@ -0,0 +1,17 @@
+package LessonOneSimpleTicTacToe;
+
+public class MainTictacToe {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+
+
+
+}
+
+
+
+
+
