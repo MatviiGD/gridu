643d2600bdb0cc8a99c58d6bfe9a9770a61ce693
Matvii Fedyk <mfedyk@griddynamics.com>
2022-03-16 14:15:55 +0200

    project first all code

---
+++ src/LessonOneSimpleTicTacToe/Coordinates.java
@@ -0,0 +1,15 @@
+package LessonOneSimpleTicTacToe;
+
+
+class Coordinates {
+    public final int x;
+    public final int y;
+
+    Coordinates(int x, int y) throws OutBoundsMoveException {
+        if (x < 0 || x > 2 || y < 0 || y > 2) {
+            throw new OutBoundsMoveException();
+        }
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/LessonOneSimpleTicTacToe/OutBoundsMoveException.java
@@ -0,0 +1,3 @@
+package LessonOneSimpleTicTacToe;
+class OutBoundsMoveException extends Exception {
+}
