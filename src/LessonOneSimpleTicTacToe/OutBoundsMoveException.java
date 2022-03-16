0f308a9bfe520d75f6e78869417f632f06675eb1
Matvii Fedyk <mfedyk@griddynamics.com>
2022-03-16 20:52:18 +0200

    added constructors, with parameters to Exceptions

--- src/LessonOneSimpleTicTacToe/OutBoundsMoveException.java
+++ src/LessonOneSimpleTicTacToe/OutBoundsMoveException.java
@@ -1,3 +1,14 @@
 package LessonOneSimpleTicTacToe;
+
 class OutBoundsMoveException extends Exception {
+
+    public OutBoundsMoveException() {
+
+    }
+
+    public OutBoundsMoveException(String message) {
+        super(message);
+
+    }
+
 }
