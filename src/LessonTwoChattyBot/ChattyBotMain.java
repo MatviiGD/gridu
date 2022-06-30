e648d3cde49c193ea7d86b5347e68ec15f0439de
Matvii Fedyk <mfedyk@griddynamics.com>
2022-06-30 13:37:54 +0300

    added hello world

--- src/LessonTwoChattyBot/ChattyBotMain.java
+++ src/LessonTwoChattyBot/ChattyBotMain.java
@@ -1,13 +1,7 @@
 package LessonTwoChattyBot;
-import java.util.Scanner;
 
 public class ChattyBotMain {
-
-
     public static void main(String[] args) {
-        System.out.println("Fuck u!213");
+        System.out.println("Hello World!");
     }
-
-
-
 }
