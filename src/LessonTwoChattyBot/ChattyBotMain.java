545e279af8ff2b7491995098ad10f82e01d45f2a
Matvii Fedyk <mfedyk@griddynamics.com>
2022-07-01 15:35:17 +0300

    commit 8

--- src/LessonTwoChattyBot/ChattyBotMain.java
+++ src/LessonTwoChattyBot/ChattyBotMain.java
@@ -7,7 +7,7 @@ public class ChattyBotMain {
             System.out.println("Hello World!");
         System.out.println("Hllo W2orld!a");
         System.out.println("Hello World!b");
-        System.out.println("Hello World!c3");
+        System.out.println("Hello Wosffsdrld!c3");
         System.out.println("Hello World!gdfgc2");
         System.out.println("Hello World!c1123");
     }
